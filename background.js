290b6f72d42df1d6e6e5d885f304659d345e4eca
Code-With-4cup-Tea <codewith4cuptea@gmail.com>
2023-09-17 18:22:54 +0530

    complete back color change navigation

--- background.js
+++ background.js
@@ -4,10 +4,32 @@ const backselect = document.querySelector(".backselect");
 // console.log(backselect);
 const arrow = document.querySelector(".backhead i");
 // console.log("arrow");
+const colorinput = document.querySelector(".colorinput");
+// console.log(colorinput);
+const bodybackground = document.querySelector(".bodybackground");
+// console.log(body);
+
 
 
 backhead.addEventListener("click",()=>{
     backselect.classList.toggle("hide");
     arrow.classList.toggle("arrow");
+
+})
+// windwo se event.target wo deta hai jis html tag pe click kar rahey ho
+window.addEventListener("click",(event)=>{
+    
+    if(event.target == bodybackground){
+        // console.log(event.target)
+        backselect.classList.add("hide");
+        arrow.classList.remove("arrow");
+        // console.log(event.target)
+       
+    }
+
+})
+
+colorinput.addEventListener("input",()=>{
+    bodybackground.style.background=colorinput.value;
 })
 
