0ddcc12a540ab33714831d079e033660182f3b8c
Code-With-4cup-Tea <codewith4cuptea@gmail.com>
2023-09-24 00:01:49 +0530

    gitaccessapi

--- datastructure.js
+++ datastructure.js
@@ -1,12 +1,12 @@
 //  for print each element  1 //
 
 // var datas = [2,5,6,8,9,7];
+// var item = 5;
 // for(var i=0; i<datas.length; i++){
         
-//     document.write("each value of datas",datas[i],'<br>');
-
-//     console.log("each value of datas" , datas[i]);
+//   console.log(datas[])
 // }
+
 
 
 //  for PRINT particular element  2 //
@@ -20,19 +20,35 @@
 
 // print element using button//
 
+// var datas = [2,5,6,8,9,7];
+
+// let but = document.querySelector("button");
+// let input = document.querySelector("input");
+
+// but.addEventListener("click",()=>{
+//     alert("function call");
+//     let values = input.value;
+
+//     document.write("your element number is ", datas[values]);
+
+
+// })
+
 var datas = [2,5,6,8,9,7];
 
-let but = document.querySelector("button");
-let input = document.querySelector("input");
+let removepos = 3;
 
-but.addEventListener("click",()=>{
-    alert("function call");
-    let values = input.value;
+for(i=3;i<datas.length-1; i++){
+   
+    datas[i] = datas[i+1]; //jo bhi current position uski jagh next position wali item ko daldo 
+    // phir last 
+    
+   
+}
+datas.length = datas.length-1; //datas ki lenght ko 1 se karm kar diya yani last wala item remved
+console.log(datas)
 
-    document.write("your element number is ", datas[values]);
 
-
-})
 
 
 
---
+++ github.js
@@ -0,0 +1,60 @@
+const input = document.querySelector(".searchinput");
+const button = document.querySelector(".searchbutton");
+const card = document.querySelector(".card");
+const image = document.querySelector(".image");
+const login = document.querySelector(".login");
+const names = document.querySelector(".name");
+const url = document.querySelector(".url");
+const bio = document.querySelector(".bio");
+const followers = document.querySelector(".followers");
+const following = document.querySelector(".following");
+const public_repos = document.querySelector(".public_repos");
+
+
+var toggle = false;
+
+
+
+const urls = "https://api.github.com/users";
+const profile = async(inputvalue)=>{
+    try{
+        // const username = inputs();
+        const url = await fetch(`${urls}/${inputvalue}`);
+        
+        const data = await url.json();
+        console.log(data)
+        // image.innerHTML="url`${data.avatar_url}`";
+        login.innerHTML =data.login;
+        names.innerHTML =data.name;
+        url.innerHTML=data.html_url;
+        bio.innerHTML=data.bio;
+        followers.innerHTML=data.followers;
+        following.innerHTML=data.following;
+        public_repos.innerHTML=data.public_repos;
+
+
+
+
+        console.log("data",data);
+    } catch(error){
+    console.log({error})
+    }
+}
+
+
+const inputs = ()=>{
+    if(toggle){
+        card.classList.add("hide")
+    }else{
+        card.classList.remove("hide")
+        var inputvalue = input.value;
+    console.log(inputvalue)
+    return profile(inputvalue);
+    }
+    
+    
+    
+}
+
+button.addEventListener("click",inputs);
+console.log("input call hoga")
