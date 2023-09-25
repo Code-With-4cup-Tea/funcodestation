c0dd740ee1979dabd0b74db4eed3deb0df3ce3f5
Code-With-4cup-Tea <codewith4cuptea@gmail.com>
2023-09-25 17:03:57 +0530

    git api with responsive

--- github.js
+++ github.js
@@ -2,11 +2,12 @@ const input = document.querySelector(".searchinput");
 const button = document.querySelector(".searchbutton");
 const card = document.querySelector(".card");
 const image = document.querySelector(".image");
+console.log(image.src);
 const login = document.querySelector(".login");
 const names = document.querySelector(".name");
 const url = document.querySelector(".url");
-const bio = document.querySelector(".bio");
-const followers = document.querySelector(".followers");
+const id = document.querySelector(".id");
+const follower = document.querySelector(".follower");
 const following = document.querySelector(".following");
 const public_repos = document.querySelector(".public_repos");
 
@@ -27,12 +28,12 @@ const profile = async(inputvalue)=>{
         login.innerHTML =data.login;
         names.innerHTML =data.name;
         url.innerHTML=data.html_url;
-        bio.innerHTML=data.bio;
-        followers.innerHTML=data.followers;
+        id.innerHTML=data.id;
+        follower.innerHTML=data.followers;
         following.innerHTML=data.following;
         public_repos.innerHTML=data.public_repos;
-
-
+        Image.src=`${data.avatar_url}`;
+    
 
 
        
