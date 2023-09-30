abde0c841d80fb6226e841333901e1320e689a79
Code-With-4cup-Tea <codewith4cuptea@gmail.com>
2023-09-30 10:28:38 +0530

    update todo

--- exp.js
+++ exp.js
@@ -60,15 +60,39 @@ const rendertransaction = ()=>{ //16 ab jo data mill raha usay ui pe show karega
        const sign = iscredit  ? "+" : "-";   //22: iscredit ture hai to sign main + a jaye warna -
        const cd = iscredit ? "C" : "D";  // 23:yadi credit ture hia to c , cd main store kar do warna d
             const pasteinhtml = `
-        <div class="transaction" id= "${id}">
-              <div class="left">
-                <p>${text}</p>
-                <p>${sign} ₹ ${amount}</p>
-                </div>
-                <div class="status ${iscredit ? "credit" : "debit" }">${cd}</div>
-        </div>
+            <div class="transaction" id= "${id}">
+                <div class="content" onclick=showEdit(${id})>
+            <div class="left">
+            <p>${text}</p>
+            <p>${sign} ₹ ${amount}</p>
+            </div>
+            <div class="status ${iscredit ? "credit" : "debit" }">${cd}</div>
+            </div>
+            <div class="lower hide">
+             <div class="icon">
+              <i class="ri-pencil-fill" onclick=handleupdate(${id})></i>
+              <i class="ri-delete-bin-6-fill" onclick=handledel(${id})></i>
+             </div>
+            </div>
+          </div>
         
         `
+        //27: for edit or delete in html
+    //     ` <div class="transaction" id= "${id}">
+    //     <div class="content">
+    //     <div class="left">
+    //     <p>${text}</p>
+    //     <p>${sign} ₹ ${amount}</p>
+    //     </div>
+    //     <div class="status ${iscredit ? "credit" : "debit" }">${cd}</div>
+    //     </div>
+    //     <div class="lower">
+    //      <div class="icon">
+    //       <i class="ri-pencil-fill"></i>
+    //       <i class="ri-delete-bin-6-fill"></i>
+    //      </div>
+    //     </div>
+    //   </div>`
         earningamount += iscredit ? amount : 0; // earningamount , yadi iscredit true hoga to amount add ho jayga
         //warna earningamount ko 0 set kar do
 
@@ -129,6 +153,34 @@ const addamount = (e)=>{
     // console.log(data) 6: data name ke object main jo value store hui usay print karega
 
 }
+//27: for when click on edit than open ye function call hoga upar content se waah se id ayaga
+
+const showEdit =(id) =>{
+const selectedTransaction = document.getElementById(id); //28: id ka use karkey wo element milega
+const lowerEl = selectedTransaction.querySelector(".lower") // 29: 28 main jo element mila hai usmay se .lower class wala element select karegay
+lowerEl.classList.toggle("hide"); //30: click karnay pe deletel or edit wala deikega or band hoga
+}
+
+
+const handledel = (id)=>{   //31:for delete
+const filterd = state.transaction.filter((t)=>t.id == id)    //30: state ke ander jo transation object array usamy say
+                                                        //31 > jo id match karegi jo id upaar se ayagi click karnay pe usako chor ke sabko filterd main array ke roop main dal dega
+                                               
+            state.transaction = filterd; //32: jo filterd main array mill usko dubara se hum sated ke transaction main asign kar diya to wahi dekgea jo hai delete kiya hua nahi
+            rendertransaction(); // 33: function ko dubara call kar diya taki update ho jaye
+
+}
+
+
+const handleupdate = (id)=>{   //31:for delete
+            const transaction = state.transaction.find((t)=>t.id == id) //34: ye same id ke trasnsation ko dundhega
+    const{text,amount} = transaction;   //35: text or ammount
+// console.log(text,amount);
+const textinput = document.getElementById("text"); //36: text ko select kiya
+const amountinput=document.getElementById("amount"); //37:amount ko select kiya
+textinput.value = text; //38: text ki value pass
+amountinput.value = amount; //39: amount value  pass
+        }
 rendertransaction(); //26 : taki jo pheley se value tha html main wo hat jaye
 transactionforms.addEventListener("submit",addamount); //1 : from pe even listner lagaya hai or jo
 //value submit hogi usay addamount ke fucntion main store kia 
