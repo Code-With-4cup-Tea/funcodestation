c59ff3162873c8242851a428a639e3c8aee568e6
Code-With-4cup-Tea <codewith4cuptea@gmail.com>
2023-10-29 11:49:23 +0530

    complete without storage

--- jerraticket/script.js
+++ jerraticket/script.js
@@ -1,6 +1,6 @@
 
 const add = document.querySelector(".addbtn");
-const removebtn = document.querySelector(".removebtn")
+// const removebtn = document.querySelector(".removebtn")
 // console.log(add);
 const ticketbox = document.querySelector(".ticketbox")
 const mainticketcontainer = document.querySelector(".mainticketcontainer");
@@ -9,9 +9,12 @@ const priorcolors = document.querySelectorAll(".pcolors")
 // console.log(priorcolors)
 const topcolor = document.querySelectorAll(".colors")
 // console.log(topcolor)
+const maincontain =document.querySelectorAll(".maincontain")
+const search = document.querySelector(".search");
 
-let ticketcolorArr = [];
-console.log(ticketcolorArr);
+
+
+
 
 let colors = ["rgb(2, 39, 2)"," rgb(2, 85, 2)"," rgb(4, 167, 4)"," rgb(10, 235, 10)"];
 let boxprioritycolor = colors[colors.length-1]
@@ -19,6 +22,7 @@ let id = 1;
 let colorId = colors[id]
 // console.log(colorId)
 // console.log(id)
+const arr = [];
 
 
 let flag = false;
@@ -45,11 +49,6 @@ if(flag){
 }
 })
 
-//for remove 
-removebtn.addEventListener("click",(e)=>{
-     removeFlag = !removeFlag;
-     console.log(removeFlag)
-})
 
 //for priority color
 priorcolors.forEach((colorvalue,index)=>{
@@ -70,22 +69,29 @@ priorcolors.forEach((colorvalue,index)=>{
 
 //ticket creator function
 
-const createTicket = (ticketcolor,taskid,task)=>{
+const createTicket = (ticketcolor,id,taskid,task)=>{
     const ticketcont = document.createElement("div");
+    arr.push(ticketcont);
     ticketcont.setAttribute("class","ticket-cont");
     ticketcont.innerHTML =`
     <div class="maincontain ">
-    <div class="ticketcolor" style="background-color:${ticketcolor};"></div>
+    <div class="ticketcolor" style="background-color:${ticketcolor};">${id}</div>
     <div class="taskid">Task_Id:#${taskid}</div>
     <div class="task">${task}</div>
     <div class="lock"><i class="fa-solid fa-lock"></i></div>
+    <div class="removebtn">
+     <i class="fa-solid fa-xmark fa-fade" style="color: #ff0000;"></i>
+    </div>
    </div>
     `
-    ticketcolorArr.push(ticketcolor,taskid,task);
    
+    console.log(arr)
+
+    
 
     mainticketcontainer.append(ticketcont)
-    handleRemovel(ticketcont); //passing ticket which created for remove
+     //passing ticket which created for remove
+     handleRemovel(ticketcont)
     lockunlock(ticketcont);
     ticketStripColorChange(ticketcont)
 
@@ -99,17 +105,47 @@ ticketbox.addEventListener("keydown",(e)=>{
      let key = e.key; //this give key which press
 
      if(key === "Shift"){
-        createTicket(colorId,uniqid(),textarea.value);
+        createTicket(colorId,id,uniqid(),textarea.value);
         ticketbox.style.display="none"
         flag = false;
         textarea.value="";
      }
 })
 
-function handleRemovel(ticketcont){
-    if(removeFlag) ticketcont.remove();
 
+
+//for remove 
+// removebtn.addEventListener("click",handleRemovel)
+            // removeFlag = !removeFlag;
+            // handleRemovel(ticketcont);
+            // console.log(removeFlag)
+
+
+
+
+const handleRemovel= (ticketcont)=>{
+    // console.log(ticketcont)
+    const list =ticketcont.querySelector(".removebtn");
+// console.log(removeFlag)
+    // if(removeFlag){
+        list.addEventListener("click",()=>{
+            ticketcont.remove();
+            console.log(removeFlag)
+        })
+    // } 
+
+    
+    console.log(list)
+
+    // e.target.parentElement.remove();
 }
+   
+    
+      
+     
+    
+
+
 // for lock or unlock
 const lockunlock = (ticketcont)=>{
      const lockdiv = ticketcont.querySelector(".lock")
@@ -122,12 +158,16 @@ const lockunlock = (ticketcont)=>{
         if(falock.classList.contains(lock)){
             falock.classList.remove(lock);
             falock.classList.add(unlock)
+            // removeFlag = !removeFlag
+            // console.log(removeFlag)
             //for editable area
             taskarea.setAttribute("contenteditable","true")
         }else{
             falock.classList.remove(unlock)
             falock.classList.add(lock)
             //for disable edit 
+            // removeFlag= !removeFlag
+            // console.log(removeFlag)
             taskarea.setAttribute("contenteditable","false")
         }
        
@@ -154,8 +194,11 @@ const ticketStripColorChange = (ticketcont)=>{
        id++;
        const ids = id%colors.length;
        colorId = colors[ids];
+       
+    //    console.log(ids)
     //    console.log(colorId)
        colorstrip.style.background=colorId;
+       colorstrip.innerHTML=ids;
     })
 }
 
@@ -175,7 +218,21 @@ const ticketStripColorChange = (ticketcont)=>{
 // const rgbcolor = colors.filter(rgb);
 // console.log(rgbcolor)
 
-topcolor.forEach((topvalue,index)=>{
-    const topclass = topvalue.classList[0];
-    console.log(topclass)
+// topcolor.forEach((topvalue,index)=>{
+//     const topclass = topvalue.classList[0];
+//     console.log(topclass)
+// })
+
+search.addEventListener("input",(e)=>{
+
+    const val = e.target.value;    
+
+    arr.map((it)=>{
+        // const d =it.innerText;
+        console.log(it)
+        it.innerText.toLowerCase().includes(val.toLowerCase()) ?
+
+        it.classList.remove("hide"):it.classList.add("hide")
+    })
+
 })
